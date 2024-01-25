eebbff88541f17874d7f9c0ee7bcf555e2c7e1b9
ruleeeer <70385062+ruleeeer@users.noreply.github.com>
2024-01-25 11:09:47 +0800

    chore: delete unused script and format

--- packages/tools/benchmark.mjs
+++ packages/tools/benchmark.mjs
@@ -1,9 +1,9 @@
-import { appendFileSync, readFileSync, writeFileSync } from "fs";
+import {appendFileSync, readFileSync, writeFileSync} from "fs";
 import playwright from "playwright";
-import { parseArgs } from './parseArgs.mjs'
-import { buildTools } from "./buildTools.mjs"
+import {parseArgs} from './parseArgs.mjs'
+import {buildTools} from "./buildTools.mjs"
 import {fileURLToPath} from "node:url";
-import {resolve , dirname}from "path";
+import {resolve, dirname} from "path";
 
 
 // TODO 增加vue的workspace名称 通过参数传入
@@ -17,10 +17,10 @@ const originalRootFileContent = readFileSync(rootFilePath, 'utf-8');
 const originalLeafFileContent = readFileSync(leafFilePath, 'utf-8');
 
 const {
-  type,
-  count,
-  hotRun,
-  outputMd
+    type,
+    count,
+    hotRun,
+    outputMd
 } = parseArgs()
 const runDev = type === 'all' || type === 'dev'
 const runBuild = type === 'all' || type === 'build'
@@ -31,171 +31,175 @@ console.log()
 const results = []
 
 if (runDev) {
-  const browser = await playwright.chromium.launch();
+    const browser = await playwright.chromium.launch();
 
-  for (const buildTool of buildTools) {
-    const totalResult = {}
+    for (const buildTool of buildTools) {
+        const totalResult = {}
 
-    if (hotRun) {
-      console.log(`Populate cache: ${buildTool.name}`);
-      const page = await (await browser.newContext()).newPage();
-      await buildTool.startServer(workspaceName);
-      await page.goto(`http://localhost:${buildTool.port}`, { waitUntil: 'load' });
-      buildTool.stop();
-      await page.close();
-    }
-
-    for (let i = 0; i < count; i++) {
-      try {
-        console.log(`Running: ${buildTool.name} (${i+1})`);
-
-        if (!hotRun) {
-          await buildTool.clean?.()
+        if (hotRun) {
+            console.log(`Populate cache: ${buildTool.name}`);
+            const page = await (await browser.newContext()).newPage();
+            await buildTool.startServer(workspaceName);
+            await page.goto(`http://localhost:${buildTool.port}`, {waitUntil: 'load'});
+            buildTool.stop();
+            await page.close();
         }
 
-        const page = await (await browser.newContext()).newPage();
-        await new Promise((resolve) => setTimeout(resolve, 300)); // give some rest
+        for (let i = 0; i < count; i++) {
+            try {
+                console.log(`Running: ${buildTool.name} (${i + 1})`);
 
-        const loadPromise = page.waitForEvent('load',{timeout:300000});
-        const pageLoadStart = Date.now();
-        const serverStartTime = await buildTool.startServer(workspaceName);
-        page.goto(`http://localhost:${buildTool.port}`);
-        await loadPromise;
-        totalResult.startup ??= 0;
-        totalResult.startup += (Date.now() - pageLoadStart);
-        if (serverStartTime !== null) {
-          totalResult.serverStart ??= 0;
-          totalResult.serverStart += serverStartTime;
-        }
+                if (!hotRun) {
+                    await buildTool.clean?.()
+                }
 
-        await new Promise((resolve) => setTimeout(resolve, 500));
+                const page = await (await browser.newContext()).newPage();
+                await new Promise((resolve) => setTimeout(resolve, 300)); // give some rest
 
-        if(!buildTool.skipHmr){
-          const rootConsolePromise = page.waitForEvent('console', { timeout:300000, predicate: e => {
-            const logText = e.text();
-            console.log(logText)
-            return logText.includes('root hmr');
-          }});
-          appendFileSync(rootFilePath, `
+                const loadPromise = page.waitForEvent('load', {timeout: 300000});
+                const pageLoadStart = Date.now();
+                const serverStartTime = await buildTool.startServer(workspaceName);
+                page.goto(`http://localhost:${buildTool.port}`);
+                await loadPromise;
+                totalResult.startup ??= 0;
+                totalResult.startup += (Date.now() - pageLoadStart);
+                if (serverStartTime !== null) {
+                    totalResult.serverStart ??= 0;
+                    totalResult.serverStart += serverStartTime;
+                }
+
+                await new Promise((resolve) => setTimeout(resolve, 500));
+
+                if (!buildTool.skipHmr) {
+                    const rootConsolePromise = page.waitForEvent('console', {
+                        timeout: 300000, predicate: e => {
+                            const logText = e.text();
+                            console.log(logText)
+                            return logText.includes('root hmr');
+                        }
+                    });
+                    appendFileSync(rootFilePath, `
             console.log('root hmr');
           `)
-          const hmrRootStart = Date.now();
-          await rootConsolePromise;
-          totalResult.rootHmr ??= 0;
-          totalResult.rootHmr += (Date.now() - hmrRootStart);
+                    const hmrRootStart = Date.now();
+                    await rootConsolePromise;
+                    totalResult.rootHmr ??= 0;
+                    totalResult.rootHmr += (Date.now() - hmrRootStart);
 
-          await new Promise((resolve) => setTimeout(resolve, 500));
+                    await new Promise((resolve) => setTimeout(resolve, 500));
 
-          const leafConsolePromise = page.waitForEvent('console', { timeout:300000, predicate: e => {
-            const logText = e.text();
-            console.log(logText)
-            return logText.includes('leaf hmr');
-          } });
-          appendFileSync(leafFilePath, `
+                    const leafConsolePromise = page.waitForEvent('console', {
+                        timeout: 300000, predicate: e => {
+                            const logText = e.text();
+                            console.log(logText)
+                            return logText.includes('leaf hmr');
+                        }
+                    });
+                    appendFileSync(leafFilePath, `
             console.log('leaf hmr');
           `)
-          const hmrLeafStart = Date.now();
-          await leafConsolePromise;
-          totalResult.leafHmr ??= 0;
-          totalResult.leafHmr += (Date.now() - hmrLeafStart);
-      }
+                    const hmrLeafStart = Date.now();
+                    await leafConsolePromise;
+                    totalResult.leafHmr ??= 0;
+                    totalResult.leafHmr += (Date.now() - hmrLeafStart);
+                }
 
-      buildTool.stop();
-      await page.close();
-      } finally {
-        writeFileSync(rootFilePath, originalRootFileContent);
-        writeFileSync(leafFilePath, originalLeafFileContent);
-      }
+                buildTool.stop();
+                await page.close();
+            } finally {
+                writeFileSync(rootFilePath, originalRootFileContent);
+                writeFileSync(leafFilePath, originalLeafFileContent);
+            }
+        }
+
+        const result = Object.fromEntries(Object.entries(totalResult).map(([k, v]) => [k, v ? (v / count).toFixed(1) : v]))
+        results.push({name: buildTool.name, result})
     }
 
-    const result = Object.fromEntries(Object.entries(totalResult).map(([k, v]) => [k, v ? (v / count).toFixed(1) : v]))
-    results.push({ name: buildTool.name, result })
-  }
-
-  await browser.close();
+    await browser.close();
 }
 
 if (runBuild) {
-  for (const buildTool of buildTools) {
-    if (buildTool.buildScript) {
-      await buildTool.clean?.()
+    for (const buildTool of buildTools) {
+        if (buildTool.buildScript) {
+            await buildTool.clean?.()
 
-      let sum = 0;
-      for (let i = 0; i < count; i++) {
-        console.log(`Running: ${buildTool.name} (${i+1})`);
-        const productionStart = Date.now();
-        await buildTool.startProductionBuild(workspaceName)
-        const productionEnd = Date.now()
-        sum += productionEnd - productionStart
-      }
+            let sum = 0;
+            for (let i = 0; i < count; i++) {
+                console.log(`Running: ${buildTool.name} (${i + 1})`);
+                const productionStart = Date.now();
+                await buildTool.startProductionBuild(workspaceName)
+                const productionEnd = Date.now()
+                sum += productionEnd - productionStart
+            }
 
-      const matchedResult = results.find((item) => item.name === buildTool.name)
-      if (matchedResult) {
-        matchedResult.result.production = (sum / count).toFixed(1);
-      } else {
-        results.push({ name: buildTool.name, result: { production: (sum / count).toFixed(1) } })
-      }
+            const matchedResult = results.find((item) => item.name === buildTool.name)
+            if (matchedResult) {
+                matchedResult.result.production = (sum / count).toFixed(1);
+            } else {
+                results.push({name: buildTool.name, result: {production: (sum / count).toFixed(1)}})
+            }
+        }
     }
-  }
 }
 
 console.log('-----')
 console.log('Results')
 
 if (outputMd) {
-  const rows = [
-    'name',
-    ...(runDev ? ['startup', 'Root HMR', 'Leaf HMR'] : []),
-    ...(runBuild ? ['Build time'] : [])
-  ]
-  let out = `| ${rows.join(' | ')} |\n`
-  out += `| ${rows.map((v, i) => i === 0 ? ' --- ' : ' ---: ').join('|')} |\n`
-  out += results
-    .map(
-      ({ name, result }) =>
-        `| ${[
-          name,
-          ...(runDev
-            ? [
-                `${result.startup}ms${
-                  result.serverStart
-                    ? ` (including server start up time: ${result.serverStart}ms)`
-                    : ''
-                }`,
-                `${result.rootHmr ? `${result.rootHmr}ms` : '---'}`,
-                `${result.leafHmr ? `${result.leafHmr}ms` : '---'}`
-              ]
-            : []),
-          ...(runBuild
-            ? [`${result.production ? `${result.production}ms` : '---'}`]
-            : []
-          )
-        ].join(' | ')} |`
-    )
-    .join('\n')
-  console.log(out)
+    const rows = [
+        'name',
+        ...(runDev ? ['startup', 'Root HMR', 'Leaf HMR'] : []),
+        ...(runBuild ? ['Build time'] : [])
+    ]
+    let out = `| ${rows.join(' | ')} |\n`
+    out += `| ${rows.map((v, i) => i === 0 ? ' --- ' : ' ---: ').join('|')} |\n`
+    out += results
+        .map(
+            ({name, result}) =>
+                `| ${[
+                    name,
+                    ...(runDev
+                        ? [
+                            `${result.startup}ms${
+                                result.serverStart
+                                    ? ` (including server start up time: ${result.serverStart}ms)`
+                                    : ''
+                            }`,
+                            `${result.rootHmr ? `${result.rootHmr}ms` : '---'}`,
+                            `${result.leafHmr ? `${result.leafHmr}ms` : '---'}`
+                        ]
+                        : []),
+                    ...(runBuild
+                            ? [`${result.production ? `${result.production}ms` : '---'}`]
+                            : []
+                    )
+                ].join(' | ')} |`
+        )
+        .join('\n')
+    console.log(out)
 } else {
-  const out = Object.fromEntries(results.map(({ name, result }) => [
-    name,
-    {
-      ...(runDev
-        ? {
-            'startup time': `${result.startup}ms${
-              result.serverStart
-                ? ` (including server start up time: ${result.serverStart}ms)`
-                : ''
-            }`,
-            'Root HMR time': `${result.rootHmr ? `${result.rootHmr}ms` : '---'}`,
-            'Leaf HMR time': `${result.leafHmr ? `${result.leafHmr}ms` : '---'}`,
-          }
-        : {}),
-      ...(runBuild
-        ? {
-          'Build time': `${result.production ? `${result.production}ms` : '---'}`
+    const out = Object.fromEntries(results.map(({name, result}) => [
+        name,
+        {
+            ...(runDev
+                ? {
+                    'startup time': `${result.startup}ms${
+                        result.serverStart
+                            ? ` (including server start up time: ${result.serverStart}ms)`
+                            : ''
+                    }`,
+                    'Root HMR time': `${result.rootHmr ? `${result.rootHmr}ms` : '---'}`,
+                    'Leaf HMR time': `${result.leafHmr ? `${result.leafHmr}ms` : '---'}`,
+                }
+                : {}),
+            ...(runBuild
+                    ? {
+                        'Build time': `${result.production ? `${result.production}ms` : '---'}`
+                    }
+                    : {}
+            )
         }
-        : {}
-      )
-    }
-  ]))
-  console.table(out)
+    ]))
+    console.table(out)
 }
