7f86e8092447aa938b9977c8feec169d090cd487
ruleeeer <70385062+ruleeeer@users.noreply.github.com>
2024-01-27 17:42:13 +0800

    chore: webpack config file suffix cjs=>js

---
+++ packages/projects/triangle/webpack.config.js
@@ -0,0 +1,88 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import reactRefresh from 'react-refresh/babel';
+import {resolve, dirname} from 'node:path'
+import {fileURLToPath} from "node:url";
+
+const dir = dirname(resolve(fileURLToPath(import.meta.url)));
+// webpack.config.js
+export default (env, argv) => ({
+    entry: './src/index.tsx',
+    output: {
+        path: resolve(dir, './dist-webpack')
+    },
+    resolve: {
+        extensions: ['.tsx', '.jsx', '.ts', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            ['@babel/preset-react', {runtime: "automatic"}],
+                            '@babel/preset-typescript'
+                        ],
+                        plugins: argv.mode !== 'production' ? [reactRefresh] : []
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            ['@babel/preset-react', {runtime: "automatic"}]
+                        ],
+                        plugins: argv.mode !== 'production' ? [reactRefresh] : []
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset'
+            }
+        ]
+    },
+    devServer: {
+        port: 8081,
+        hot: true
+    },
+    devtool:
+        argv.mode === 'production'
+            ? false
+            : 'eval-nosources-cheap-module-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.webpack.html'
+        }),
+        argv.mode !== 'production' && new ReactRefreshWebpackPlugin(),
+        new MiniCssExtractPlugin()
+    ],
+    optimization: {
+        minimizer: [
+            '...',
+            new CssMinimizerPlugin(),
+        ]
+    },
+
+    experiments: {
+        futureDefaults: true,
+        css: false,
+    },
+    node: {
+        global: false,
+    },
+})
---
+++ packages/projects/triangle/webpack.swc.config.js
@@ -0,0 +1,102 @@
+import TerserPlugin from 'terser-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
+import {resolve, dirname} from 'node:path'
+import {fileURLToPath} from "node:url";
+
+const dir = dirname(resolve(fileURLToPath(import.meta.url)));
+
+const generateSwcOptions = (syntax, isProd) => ({
+    jsc: {
+        parser: {
+            syntax,
+            jsx: true,
+            dynamicImport: true,
+            privateMethod: true,
+            functionBind: true,
+            classPrivateProperty: true,
+            exportDefaultFrom: true,
+            exportNamespaceFrom: true,
+            decorators: true,
+            decoratorsBeforeExport: true,
+            importMeta: true,
+        },
+        transform: {
+            react: {
+                runtime: "automatic",
+                development: !isProd,
+                refresh: !isProd,
+            },
+        },
+    },
+})
+
+// webpack.config.js
+export default (env, argv) => ({
+    entry: './src/index.tsx',
+    output: {
+        path: resolve(dir, './dist-webpack-swc')
+    },
+    resolve: {
+        extensions: ['.tsx', '.jsx', '.ts', '.js', '.json']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: {
+                    loader: 'swc-loader',
+                    options: generateSwcOptions('typescript', argv.mode === 'production'),
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: 'swc-loader',
+                    options: generateSwcOptions('ecmascript', argv.mode === 'production'),
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset'
+            }
+        ]
+    },
+    devServer: {
+        port: 8082,
+        hot: true
+    },
+    devtool:
+        argv.mode === 'production'
+            ? false
+            : 'eval-nosources-cheap-module-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.webpack.html'
+        }),
+        argv.mode !== 'production' && new ReactRefreshWebpackPlugin(),
+        new MiniCssExtractPlugin()
+    ],
+    optimization: {
+        minimize: argv.mode === 'production',
+        minimizer: [
+            new TerserPlugin({minify: TerserPlugin.swcMinify}),
+            new CssMinimizerPlugin(),
+        ],
+    },
+
+    experiments: {
+        futureDefaults: true,
+        css: false,
+    },
+    node: {
+        global: false,
+    },
+})
